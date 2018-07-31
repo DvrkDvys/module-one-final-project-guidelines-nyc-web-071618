6200017c3dce6b068c93ba6d21832ec186054e86
Andrew Och <andrewoch95@gmail.com>
2018-07-31 13:12:25 -0400

    Started making user and account classes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   belongs_to :account
   has_many :views
   has_many :shows, through: :views
+
+  def self.add_user(username, account_id)
+    User.create(username: username, account_id: account_id)
+  end
+
 end
--- db/migrate/001_create_accounts.rb
+++ db/migrate/001_create_accounts.rb
@@ -4,7 +4,7 @@ class CreateAccounts < ActiveRecord::Migration[4.2]
     create_table :accounts do |t|
       t.string :email
       t.string :password
-      t.integer :cc
+      t.string :cc
     end
   end
 end
